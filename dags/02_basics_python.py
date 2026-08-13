3b3b2eb8aa706006a6ca7def9e2a15db98d73afb
LES0915 <silver_lives@naver.com>
2026-08-13 09:27:15 +0900

    Feat: 서울 시간대 작업 시간 보정

--- dags/02_basics_python.py
+++ dags/02_basics_python.py
@@ -18,7 +18,7 @@ KST = pendulum.timezone("Asia/Seoul")
 def _extract_cb(**kwargs):
   '''
   - kwargs : airflow가 작업하기 전에 내부 정보(context)를 접근할수 있는 내용 엔트리포인트
-    - context : airflow가 주입한 정보(task 정보, 시간, ... 메타 정보). task 전달데이터    
+  - context : airflow가 주입한 정보(task 정보, 시간, ... 메타 정보). task 전달데이터    
   '''
   # 1. 컨텍스트(airflow 내부에 정보 저장공간)에서 특정 정보 추출, 키는 정의 되어 있음
   #    ti : <TaskInstance: 02_basics_python.extract_task manual__2026-08-12T06:37:53.765360+00:00 [running]>
@@ -29,7 +29,9 @@ def _extract_cb(**kwargs):
   # 시간 보정
   logical_date     = kwargs["logical_date"]
   logical_date_kst = logical_date.in_timezone(KST)
-  
+  # 차후 s3등에 적제할때 파티션시 활용함 yyyy, mm, dd 끊어서 사용시 활용
+  ds_kst           = logical_date_kst.format("YYYY-MM-DD")
+  ds_nodash_kst    = logical_date_kst.format("YYYYMMDD")
 
   logging.info("=== Extract 작업 ===")
   logging.info(f" ti = {ti}")
@@ -40,6 +42,8 @@ def _extract_cb(**kwargs):
   '''
   logging.info(f" ds = {ds}")
   logging.info(f" ds_nodash = {ds_nodash}")
+  logging.info(f" ds_kst = {ds_kst}")
+  logging.info(f" ds_nodash_kst = {ds_nodash_kst}")
   logging.info(f" run_id    = {run_id}")
 
   # 정보 전달 -> XCOM 게시판에 본 task가 글을 작성하는것
