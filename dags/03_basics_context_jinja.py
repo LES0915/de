93227eee0a707996e505fc3665edb8e5846f8bf1
LES0915 <silver_lives@naver.com>
2026-08-13 09:41:16 +0900

    Feat: 틀 구성 완료

---
+++ dags/03_basics_context_jinja.py
@@ -0,0 +1,51 @@
+'''
+- airflow 내부 정보 접근, 출력시 jinja 활용, 내부 정보 접근시 macro 활용
+'''
+# 1. 모듈
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+import logging
+import pendulum
+
+# 2. 전역변수
+KST = pendulum.timezone("Asia/Seoul")
+
+# 4-1. 콜백함수
+def _print(**kwargs):
+  pass
+
+# 3. DAG
+with DAG( 
+  dag_id      = "03_basics_context_jinja",
+  description = "macro을 이용하여 context 접근, jinja를 통해 표현",
+  default_args= {
+    "owner"           : "aic-de1-admin",    
+    "retries"         : 1,
+    "retry_delay"     : timedelta(minutes=1)
+  },
+  # 매일 오전 9시 00분에 스케줄 작동
+  schedule_interval = "0 9 * * *", # cron 방식으로 표기 (분, 시, 일, 월, 주)
+  # 수행 시작 시간 서울 시간대 타임존 조정
+  start_date  = pendulum.datetime( 2026,6,29, tz=KST ),
+  catchup     = False,
+  tags        = ['macro', 'context', 'jinja']
+) as dag:
+
+  # 4. 오퍼레이터를 이용하여 task를 정의
+  t1 = BashOperator(
+    task_id         = "jinja_used_task",
+    bash_command    = ""
+  )
+  t2 = BashOperator(
+    task_id         = "jinja_macro_task",
+    bash_command    = ""
+  )
+  t3 = PythonOperator(
+    task_id         = "jinja_python_task",
+    python_callable = _print
+  )
+
+  # 5. 의존성(수행순서)
+  t1 >> t2 >> t3
